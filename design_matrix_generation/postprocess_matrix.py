830728ada70d3c459dc6f0e9c69eb4ac2a67851f
jakub <j.d.siemaszko@student.tudelft.nl>
2025-01-19 14:52:00 +0100

    final touches

--- design_matrix_generation/postprocess_matrix.py
+++ design_matrix_generation/postprocess_matrix.py
@@ -1,21 +1,30 @@
 import pandas as pd
 import numpy as np
+from itertools import product
 
 # points_per_block = 10
-de_setting_0 = 0  # starting setting
+de_setting_0 = -10  # starting setting
 de_setting_1 = 10
-obligatory_testpoints = [np.array([8, 1.6, 0, 40]).reshape(4, 1), np.array([8, 0, 0, 40]).reshape(4, 1)]
+obligatory_testpoints = [np.array([8, 1.6, de_setting_0, 40]).reshape(4, 1), np.array([8, 0, de_setting_0, 40]).reshape(4, 1)]
 
 V_base = 40
 V_diff = 20
 
 n_different_Re = 8
 
-N_validation = 5
+N_validation = 2 # per corner point
 
-alpha_range = (-5, 7, 1)
-J_range = (1.6, 2.4, 0.2)
-deltae_range = (0, 10, 10)
+alpha_range = (-4, 7, 0.5)
+J_range = (1.6, 2.4, 0.1)
+deltae_range = (-10, 10, 10)
+
+# Generate the corner points
+corner_points = []
+for r in [alpha_range, J_range, deltae_range]:
+    corner_points.append([r[0], r[1]])
+
+# Create all combinations of corner points
+corner_combinations = np.array(list(product(*corner_points)))
 
 np.random.seed(1)
 
@@ -73,21 +82,47 @@ def postprocess_matrix(path):
             main_block_one = np.concatenate((main_block_one, obligatory_testpoint), axis=1)
 
     # validation points
-    alpha_v = np.random.choice(np.arange(alpha_range[0], alpha_range[1] + alpha_range[2], alpha_range[2]), size=N_validation)
-    J_v = np.random.choice(np.arange(J_range[0], J_range[1] + J_range[2], J_range[2]), size=N_validation)
-    delta_e_v = np.random.choice(np.arange(deltae_range[0], deltae_range[1] + deltae_range[2], deltae_range[2]), size=N_validation)
-    V_v = np.full(N_validation, V_base)
+    # alpha_v = np.random.choice(np.arange(alpha_range[0], alpha_range[1] + alpha_range[2], alpha_range[2]), size=N_validation)
+    # J_v = np.random.choice(np.arange(J_range[0], J_range[1] + J_range[2], J_range[2]), size=N_validation)
+    # delta_e_v = np.random.choice(np.arange(deltae_range[0], deltae_range[1] + deltae_range[2], deltae_range[2]), size=N_validation)
+    # V_v = np.full(N_validation, V_base)
+    # if np.all(delta_e_v == de_setting_0) or np.all(delta_e_v == de_setting_1):
+    #     print("WARNING: only one elevator setting in validation set")
+    # validation_points = np.vstack((alpha_v, J_v, delta_e_v, V_v))
+    # vp_b1 = validation_points[:, np.where(validation_points[2, :] == de_setting_0)[0]]
+    # main_block_one = np.concatenate((main_block_one, vp_b1), axis=1)
+    # vp_b2 = validation_points[:, np.where(validation_points[2, :] == de_setting_1)[0]]
+    # main_block_two = np.concatenate((main_block_two, vp_b2), axis=1)
 
-    if np.all(delta_e_v == de_setting_0) or np.all(delta_e_v == de_setting_1):
-        print("WARNING: only one elevator setting in validation set")
 
-    validation_points = np.vstack((alpha_v, J_v, delta_e_v, V_v))
+    indices_to_duplicate = np.where(
 
-    vp_b1 = validation_points[:, np.where(validation_points[2, :] == de_setting_0)[0]]
-    main_block_one = np.concatenate((main_block_one, vp_b1), axis=1)
+        np.any(tuple(np.all(np.equal(main_block_one[:3, :], np.tile(corner_combinations[i], reps=(main_block_one.shape[1],1)).T), axis=0) for i in range(len(corner_combinations))), axis=0)
+    )[0]
+    indices_to_duplicate.sort()
 
-    vp_b2 = validation_points[:, np.where(validation_points[2, :] == de_setting_1)[0]]
-    main_block_two = np.concatenate((main_block_two, vp_b2), axis=1)
+    for k in indices_to_duplicate:
+        column_to_duplicate = main_block_one[:, k]
+        # Create two copies of the column
+        duplicated_columns = np.tile(column_to_duplicate[:, np.newaxis], (1, N_validation))
+        # Insert the duplicated columns right after the k'th column
+        main_block_one = np.insert(main_block_one, k + 1, duplicated_columns.T, axis=1)
+        indices_to_duplicate += N_validation
+
+
+    indices_to_duplicate = np.where(
+
+        np.any(tuple(np.all(np.equal(main_block_two[:3, :], np.tile(corner_combinations[i], reps=(main_block_two.shape[1],1)).T), axis=0) for i in range(len(corner_combinations))), axis=0)
+    )[0]
+    indices_to_duplicate.sort()
+
+    for k in indices_to_duplicate:
+        column_to_duplicate = main_block_two[:, k]
+        # Create two copies of the column
+        duplicated_columns = np.tile(column_to_duplicate[:, np.newaxis], (1, N_validation))
+        # Insert the duplicated columns right after the k'th column
+        main_block_two = np.insert(main_block_two, k + 1, duplicated_columns.T, axis=1)
+        indices_to_duplicate += N_validation
 
     wind_off_block = np.vstack(
         (
@@ -119,12 +154,15 @@ def postprocess_matrix(path):
 
     alpha_acoustic = np.max(unique_alphas)
     where_acoustic = np.where(
+        np.logical_and(
         np.logical_or(
             np.logical_and(
                 full_matrix[0, :] == alpha_acoustic,
                 full_matrix[2, :] == de_setting_0
             ),
             np.any(tuple(np.all(np.equal(full_matrix, obligatory_testpoints[i]), axis=0) for i in range(len(obligatory_testpoints))), axis=0)
+        ),
+        full_matrix[3, :] != 0
         )
     )[0]
 
