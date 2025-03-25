8d37bfda677cb37ef5b77098a86c48531cc82fcd
jsiemaszko <j.d.siemaszko@student.tudelft.nl>
2025-03-25 19:22:10 +0100

    kurwamac

--- constants.py
+++ constants.py
@@ -88,58 +88,48 @@ TAILARM_OVER_BTUNNEL = TAILARM / B_TUNNEL # -
 
 # MASKING THE DATA
 mask_RSM = np.hstack((
-    np.arange(0, 9, 1),
-    np.arange(11, 13, 1),
-    np.arange(15, 20, 1),
-    np.arange(22, 23, 1),
-    np.arange(26, 32, 1),
-    np.arange(33, 36, 1),
-    np.arange(45, 54, 1),
-    np.arange(54, 66, 1),
-    np.arange(70, 76, 1),
-    np.arange(80, 83, 1),
-    np.arange(87, 88, 1),
-    np.arange(92, 93, 1),
+    np.arange(0, 8, 1),
+    np.arange(10, 12, 1),
+    np.arange(14, 19, 1),
+    np.arange(21, 23, 1),
+    np.arange(25, 32, 1),
+    # np.arange(33, 36, 1),
+    np.arange(44, 53, 1),
+    np.arange(53, 64, 1),
+    np.arange(69, 74, 1),
+    np.arange(79, 82, 1),
+    np.arange(86, 87, 1),
+    np.arange(91, 92, 1),
 ))
 
 mask_validation = np.hstack((
-    np.arange(0, 9, 1),
-    np.arange(11, 13, 1),
-    np.arange(15, 20, 1),
-    np.arange(22, 24, 1),
-    np.arange(26, 32, 1),
-    np.arange(33, 36, 1),
-    np.arange(44, 54, 1),
-    np.arange(54, 66, 1),
-    np.arange(70, 76, 1),
-    np.arange(80, 83, 1),
-    np.arange(87, 88, 1),
-    np.arange(92, 93, 1),
+    np.arange(32, 35, 1),
+    np.arange(92, 95, 1),
 ))
 
 mask_repetition_exclusive = np.hstack((
-    np.arange(9, 11, 1),
-    np.arange(13, 15, 1),
-    np.arange(20, 22, 1),
-    np.arange(24, 26, 1),
-    np.arange(66, 70, 1),
-    np.arange(76, 80, 1),
-    np.arange(83, 87, 1),
-    np.arange(88, 92, 1),
+    np.arange(8, 10, 1),
+    np.arange(12, 14, 1),
+    np.arange(19, 21, 1),
+    np.arange(23, 25, 1),
+    np.arange(64, 69, 1),
+    np.arange(74, 79, 1),
+    np.arange(82, 86, 1),
+    np.arange(88, 91, 1),
 ))
 
 mask_repetition_inclusive = np.hstack((
-    np.arange(8, 11, 1),
-    np.arange(12, 15, 1),
-    np.arange(19, 22, 1),
-    np.arange(23, 26, 1),
-    np.arange(65, 70, 1),
-    np.arange(75, 80, 1),
-    np.arange(82, 87, 1),
-    np.arange(87, 92, 1),
+    np.arange(7, 10, 1),
+    np.arange(11, 14, 1),
+    np.arange(18, 21, 1),
+    np.arange(22, 25, 1),
+    np.arange(63, 69, 1),
+    np.arange(73, 79, 1),
+    np.arange(81, 86, 1),
+    np.arange(87, 91, 1),
 ))
 
 mask_low_Re = np.hstack((
-    np.arange(36, 44, 1),
+    np.arange(35, 44, 1),
 ))
 
